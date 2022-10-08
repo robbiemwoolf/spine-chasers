062f2c6b100ed24f5b02842e725dab6b05f3bb13
Robbie Woolf <robbiemwoolf@gmail.com>
2022-10-08 09:00:27 -0700

    fixed migration error

--- backend/src/db/migrations/20221008123035_createLibrary.js
+++ backend/src/db/migrations/20221008123035_createLibrary.js
@@ -1,12 +1,13 @@
 exports.up = function(knex) {
     return knex.schema.createTable('library', (table) => {
         table.increments('book_id').primary()
-        table.integer('isbn').unique().notNullable()
+        table.bigint('isbn').unique().notNullable()
         table.string('title')
         table.string('author')
         table.string('image_url')
         table.string('description')
         table.integer('rating')
+        table.specificType('tags', 'text[]')
         table.timestamps(true, true)
     })
 }
