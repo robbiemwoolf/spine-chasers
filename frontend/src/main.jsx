4b5f4f000db726bdbdb8f9b89b078c56efe5f274
Robbie Woolf <robbiemwoolf@gmail.com>
2023-05-08 10:19:00 -0700

    reprogrammed frontend with vite config

---
+++ frontend/src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Home from './home/Home'
+import ErrorPage from './errors/errorPage'
+import Header from './header/Header'
+import AllBooks from './books/AllBooks'
+import AllComics from './comics/AllComics'
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element: [<Header key='head'/>, <Home key='home'/>],
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/books",
+        element: [<Header key="head" />, <AllBooks key="book" />],
+    },
+    {
+        path: "/comics",
+        element: [<Header key="head" />, <AllComics key="comi" />],
+    }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
