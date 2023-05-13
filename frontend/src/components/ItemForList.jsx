1eac917979d7cb4818fc4c6f3f6c1c81b133c63c
Robbie Woolf <robbiemwoolf@gmail.com>
2023-05-13 15:51:27 -0700

    rudimentary start to client side layout

---
+++ frontend/src/components/ItemForList.jsx
@@ -0,0 +1,12 @@
+import './ItemForList.css'
+
+function ItemForList() {
+
+    return (
+        <div className='container'>
+            Book
+        </div>
+    )
+}
+
+export default ItemForList
--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,11 +1,20 @@
+import ItemForList from "../components/ItemForList"
+
 export default function Home() {
+    const listTitles = ['group one', 'group two', 'group three', 'group four', 'group five']
+
+    const lists = listTitles.map((listTitle) => {
+        return (
+        <>
+            <h3>{listTitle}</h3>
+            <ItemForList key={listTitle}/>
+        </>
+        )
+    })
+
     return (
         <>
-            <p>List 1</p>
-            <p>List 2</p>
-            <p>List 3</p>
-            <p>List 4</p>
-            <p>List 5</p>
+            {lists}
         </>
     )
 }
