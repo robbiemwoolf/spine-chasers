61cd9458fd9fd05bf8b59dfb6ed6a7d172486be8
Robbie Woolf <robbiemwoolf@gmail.com>
2023-05-27 07:51:41 -0700

    now fetching all books from deployed server

--- frontend/src/books/AllBooks.jsx
+++ frontend/src/books/AllBooks.jsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 export default function AllBooks() {
-    return <h1>All Books</h1>
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        async function fetchBooks() {
+            try {
+                const response = await axios.get('https://spine-chasers-server.onrender.com/library')
+                const { data } = response.data
+                setBooks(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchBooks()
+    }, [])
+
+    const loaded = books.map((book) => {
+        return (
+            <div key={book.id}>
+                <p>{book.title}</p>
+                <p>{book.isbn}</p>
+                <img src={book.image_url} alt="cover" />
+            </div>
+        )
+    })
+
+    return (
+        <>
+            <h1>All Books</h1>
+            { books ? loaded : <p>Loading...</p> }
+        </>
+    )
 }
