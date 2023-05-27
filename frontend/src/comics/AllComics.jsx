d7f2f5bc310b0042c707610d85a9600b8f690f55
Robbie Woolf <robbiemwoolf@gmail.com>
2023-05-27 07:52:12 -0700

    fetching 'graphicnovels' from server

--- frontend/src/comics/AllComics.jsx
+++ frontend/src/comics/AllComics.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
 export default function AllComics() {
-    return <h1>All Comics</h1>
+    const [comics, setComics] = useState([])
+
+    useEffect(() => {
+        async function fetchComics() {
+            try {
+                const response = await axios.get('https://spine-chasers-server.onrender.com/library?tags=graphicnovel')
+                const { data } = response.data
+                setComics(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchComics()
+    }, [])
+
+    const loaded = comics.map((comic) => {
+        return (
+            <div key={comic.id}>
+                <p>{comic.title}</p>
+                <img src={comic.image_url} alt="cover" />
+            </div>
+        )
+    })
+
+    return (
+        <>
+            <h1>All Comics</h1>
+            { comics ? loaded : <p>Loading...</p> }
+        </>
+    )
 }
