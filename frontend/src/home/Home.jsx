167f6d091b549b17b6c6c03315edc8d9d4a87d5e
Robbie Woolf <robbiemwoolf@gmail.com>
2023-06-07 15:45:46 -0700

    group one created

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,20 +1,36 @@
-import ItemForList from "../components/ItemForList"
+import { useEffect, useState } from "react"
+import axios from 'axios'
 
 export default function Home() {
-    const listTitles = ['group one', 'group two', 'group three', 'group four', 'group five']
+    const [groupOne, setGroupOne] = useState([])
 
-    const lists = listTitles.map((listTitle) => {
+    useEffect(() => {
+        async function fetchGroupOne() {
+            try {
+                const response = await axios.get('https://spine-chasers-server.onrender.com/library?tags=scifi')
+                const { data } = response.data
+                console.log(data)
+                setGroupOne(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchGroupOne()
+    }, [])
+
+    const loaded = groupOne.map((book) => {
         return (
-        <>
-            <h3>{listTitle}</h3>
-            <ItemForList key={listTitle}/>
-        </>
+            <div key={book.id}>
+                <p>{book.title}</p>
+                <p>{book.isbn}</p>
+                <img src={book.image_url} alt="cover" />
+            </div>
         )
     })
 
     return (
         <>
-            {lists}
+            { groupOne ? loaded : <p>Loading...</p> }
         </>
     )
 }
