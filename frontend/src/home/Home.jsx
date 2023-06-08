13ab41d66677a4af8bd0d22d0e55a3ee3d07a3d1
Robbie Woolf <robbiemwoolf@gmail.com>
2023-06-07 17:47:37 -0700

    initial display of five grouped lists

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
+import './Home.css'
 
 export default function Home() {
+
     const [groupOne, setGroupOne] = useState([])
+    const [groupTwo, setGroupTwo] = useState([])
+    const [groupThree, setGroupThree] = useState([])
+    const [groupFour, setGroupFour] = useState([])
+    const [groupFive, setGroupFive] = useState([])
 
     useEffect(() => {
+        const criteria = ['classics', 'horror', 'lgbtqia', 'mystery', 'nonfiction', 'lgbtq']
         async function fetchGroupOne() {
             try {
-                const response = await axios.get('https://spine-chasers-server.onrender.com/library?tags=scifi')
+                const response = await axios.get(`https://spine-chasers-server.onrender.com/library?tags=${criteria[0]}`)
                 const { data } = response.data
                 console.log(data)
                 setGroupOne(data)
@@ -15,22 +22,131 @@ export default function Home() {
                 console.error(err)
             }
         }
+        async function fetchGroupTwo() {
+            try {
+                const response = await axios.get(`https://spine-chasers-server.onrender.com/library?tags=${criteria[1]}`)
+                const { data } = response.data
+                console.log(data)
+                setGroupTwo(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        async function fetchGroupThree() {
+            try {
+                const response = await axios.get(`https://spine-chasers-server.onrender.com/library?tags=${criteria[2]}`)
+                const received = await axios.get(`https://spine-chasers-server.onrender.com/library?tags=${criteria[5]}`)
+                const { data } = response.data
+                const booksOne = data
+                const booksTwo = received.data.data
+                console.log('dataTwo: ', booksTwo)
+                console.log('dataOne: ', booksOne)
+                const dataFinal = booksOne.concat(booksTwo)
+                console.log('dataFinal:', dataFinal)
+                setGroupThree(dataFinal)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        async function fetchGroupFour() {
+            try {
+                const response = await axios.get(`https://spine-chasers-server.onrender.com/library?tags=${criteria[3]}`)
+                const { data } = response.data
+                console.log(data)
+                setGroupFour(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        async function fetchGroupFive() {
+            try {
+                const response = await axios.get(`https://spine-chasers-server.onrender.com/library?tags=${criteria[4]}`)
+                const { data } = response.data
+                console.log(data)
+                setGroupFive(data)
+            } catch (err) {
+                console.error(err)
+            }
+        }
         fetchGroupOne()
+        fetchGroupTwo()
+        fetchGroupThree()
+        fetchGroupFour()
+        fetchGroupFive()
     }, [])
 
-    const loaded = groupOne.map((book) => {
+    const first = groupOne.map((book) => {
         return (
-            <div key={book.id}>
-                <p>{book.title}</p>
-                <p>{book.isbn}</p>
-                <img src={book.image_url} alt="cover" />
+            <div key={book.id} className='coverHolder'>
+                <img className='cover' src={book.image_url} alt="cover" />
+            </div>
+        )
+    })
+    
+    const second = groupTwo.map((book) => {
+        return (
+            <div key={book.id} className='coverHolder'>
+                <img className='cover' src={book.image_url} alt="cover" />
+            </div>
+        )
+    })
+
+    const third = groupThree.map((book) => {
+        return (
+            <div key={book.id} className='coverHolder'>
+                <img className='cover' src={book.image_url} alt="cover" />
+            </div>
+        )
+    })
+
+    const fourth = groupFour.map((book) => {
+        return (
+            <div key={book.id} className='coverHolder'>
+                <img className='cover' src={book.image_url} alt="cover" />
+            </div>
+        )
+    })
+
+    const fifth = groupFive.map((book) => {
+        return (
+            <div key={book.id} className='coverHolder'>
+                <img className='cover' src={book.image_url} alt="cover" />
             </div>
         )
     })
 
     return (
         <>
-            { groupOne ? loaded : <p>Loading...</p> }
+            <div className='container'>
+                <h2 className='groupTitle'>Classics</h2>
+                <div className='group'>
+                    { groupOne ? first : <p>Loading...</p> }
+                </div>
+            </div>
+            <div className='container'>
+                <h2 className='groupTitle'>Horror</h2>
+                <div className='group'>
+                    { groupTwo ? second : <p>Loading...</p> }
+                </div>
+            </div>
+            <div className='container'>
+                <h2 className='groupTitle'>LGBTQIA+</h2>
+                <div className='group'>
+                    { groupThree ? third : <p>Loading...</p> }
+                </div>
+            </div>
+            <div className='container'>
+                <h2 className='groupTitle'>Mysteries</h2>
+                <div className='group'>
+                    { groupFour ? fourth : <p>Loading...</p> }
+                </div>
+            </div>
+            <div className='container'>
+                <h2 className='groupTitle'>Nonfiction</h2>
+                <div className='group'>
+                    { groupFive ? fifth : <p>Loading...</p> }
+                </div>
+            </div>
         </>
     )
 }
